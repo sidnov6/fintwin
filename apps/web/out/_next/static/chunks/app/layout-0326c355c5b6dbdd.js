e100b74de1ac54234d2bffaedb728d4199b18a9d
Siddharth Jain <sidnov6@gmail.com>
2026-08-31 00:30:08 +0200

    Keep generated site output out of source

--- apps/web/out/_next/static/chunks/app/layout-0326c355c5b6dbdd.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{6224:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8014,23))},8014:()=>{}},e=>{e.O(0,[893,13,511,358],()=>e(e.s=6224)),_N_E=e.O()}]);
--- apps/web/out/_next/static/chunks/pages/_app-3c36d79e883a1e3e.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{9930:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(3111)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(9930),n(3247))),_N_E=_.O()}]);
